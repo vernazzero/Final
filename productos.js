0432bc2ba926ff439b24f2871102cf5974d6f0c0
Elias <EEliasCarabajal@gmail.com>
2022-07-20 19:07:57 -0300

    creando cards

---
+++ productos.js
@@ -0,0 +1,30 @@
+ const productos = [
+    {name: "Ryzen 3 3200G", precio: "38565", img: "https://http2.mlstatic.com/D_NQ_NP_924534-MLA31852310802_082019-O.webp"},
+    {name: "Ryzen 5 3400G", precio: "51000", img: "https://http2.mlstatic.com/D_NQ_NP_662435-MLA50784234393_072022-O.webp"},
+    {name: "Ryzem 7 5800X", precio: "75000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_920739-MLA44347283255_122020-F.webp"},
+    {name: "Intel Core I3 10105f", precio: "15000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_654063-MLA46318631821_062021-F.webp"},
+    {name: "Intel Core i5-10400F", precio: "28000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_951550-MLA43003993763_082020-F.webp"},
+    {name: "Intel Core i7-10700F", precio: "59000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_767562-MLA44771865278_022021-F.webp"},
+    {name: "Motherboard Gigabyte Ga-b450m", precio: "20000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_906630-MLA50679362514_072022-F.webp"},
+    {name: "Motherboard Gigabyte H410m", precio: "15000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_607835-MLA48120448959_112021-F.webp"},
+    {name: "Memoria RAM Vengeance RGB 16GB", precio: "20000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_684969-MLA47962858193_102021-F.webp"},
+    {name: "RTX 3060 8GB MSI", precio: "135000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_862662-MLA45806911924_052021-F.webp"},
+    {name: "RX 6500XT 4GB GIGABYTE", precio: "83000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_705926-MLA49520014918_032022-F.webp"},
+    {name: "Gabinete Gamer Thermaltake S300", precio: "19900", img: "https://http2.mlstatic.com/D_NQ_NP_2X_811129-MLA44128434268_112020-F.webp"},
+    {name: "Gabinete Gamer Sentey X10", precio: "7000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_679124-MLA31652578709_082019-F.webp"},
+ ]
+
+ const containerProductos = document.getElementById("productos");
+
+ productos.forEach(producto => {
+
+    const card = document.createElement("div");
+
+    card.classList.add("card");
+
+    card.innerHTML=
+
+
+
+
+ })
