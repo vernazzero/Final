a67dc1877f101a4de666021ca96b4b1e33c6ac22
Elias <EEliasCarabajal@gmail.com>
2022-07-21 20:39:13 -0300

    100% responsive

--- productos.js
+++ productos.js
@@ -1,4 +1,5 @@
  const productos = [
+
     {name: "Ryzen 3 3200G", precio: "38565", img: "https://http2.mlstatic.com/D_NQ_NP_924534-MLA31852310802_082019-O.webp"},
     {name: "Ryzen 5 3400G", precio: "51000", img: "https://http2.mlstatic.com/D_NQ_NP_662435-MLA50784234393_072022-O.webp"},
     {name: "Ryzem 7 5800X", precio: "75000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_920739-MLA44347283255_122020-F.webp"},
@@ -12,6 +13,7 @@
     {name: "RX 6500XT 4GB GIGABYTE", precio: "83000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_705926-MLA49520014918_032022-F.webp"},
     {name: "Gabinete Gamer Thermaltake S300", precio: "19900", img: "https://http2.mlstatic.com/D_NQ_NP_2X_811129-MLA44128434268_112020-F.webp"},
     {name: "Gabinete Gamer Sentey X10", precio: "7000", img: "https://http2.mlstatic.com/D_NQ_NP_2X_679124-MLA31652578709_082019-F.webp"},
+    
  ]
 
  const containerProductos = document.getElementById("productos");
