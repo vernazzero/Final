56a5016ac784973ac979f4a952ee4cf0ea48e06e
Elias <EEliasCarabajal@gmail.com>
2022-07-22 17:51:56 -0300

    interaccion menu terminado

---
+++ main.js
@@ -0,0 +1,9 @@
+const icono = document.getElementById("menu");
+
+const menu = document.querySelector(".mostrarMenu")
+
+icono.addEventListener("click", () => {
+
+    menu.classList.toggle("menuShow");
+
+})
